6ece17437577679df96976982bd4eb25fb49e22f
Bony Roopchandani <admin@appperfect38s-Mac-mini.local>
2016-06-10 14:15:26 +0530

    fixed

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
 UPLOAD_FOLDER_AVATAR = os.path.join(basedir, 'uploads/avatar/')
 ALLOWED_EXTENSIONS = set(['gif', 'png', 'jpeg', 'jpg'])
 WTF_CSRF_ENABLED = True
